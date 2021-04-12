8907c38a5ffe48cd2bbbf9fec3d74ba862917d21
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-04-12 09:14:19 +0200

    Add pou5f1 Homo sapiens gene

--- MainExcercise.py
+++ MainExcercise.py
@@ -27,57 +27,111 @@ with open(nanog_dir + "nanog_gene.txt") as f:
     lines = "".join(f.readlines())
 f.close()
 
-# nanog Homo sapiens gene sequence
 nanog_seq = DNA(lines)
-# nanog_seq.to_string()
-# print("Is valid gene sequence? {}".format(seq_utils.is_valid_dna(nanog_seq.seq)))
 
-# Open and read nanog homo sapiends cds file
+# ========================= NANOG CDS =========================
 with open(nanog_dir + "nanog_cds.txt") as f:
     lines = "".join(f.readlines())
 f.close()
 
-# Split multiple sequence
 cds_list = file_utils.split_multiple_sequences(lines)
-
-# Convert to objects
 cds_list = list_utils.convert_list_to_DNA(cds_list)
-# cds_list[0].to_string()
-# cds_list[0].print_type()
 
-# Read nanog homo sapiens protein file
+# ========================= NANOG PROTEIN =========================
 with open(nanog_dir + "nanog_prot.txt") as f:
     lines = "".join(f.readlines())
 f.close()
 
 prot_list = file_utils.split_multiple_sequences(lines)
 prot_list = list_utils.convert_list_to_Protein(prot_list)
-# prot_list[0].to_string()
-# prot_list[0].print_type()
 
-# Read nanog homo sapiens exon file
+# ========================= NANOG EXON =========================
 with open(nanog_dir + "nanog_exon.txt") as f:
     lines = "".join(f.readlines())
 f.close()
 
 exon_list = file_utils.split_multiple_sequences(lines)
 exon_list = list_utils.convert_list_to_DNA(exon_list)
-# exon_list[0].to_string()
-# exon_list[0].print_type()
 
-# Read nanog homo sapiens transcript file
+# ========================= NANOG mRNA =========================
 with open(nanog_dir + "nanog_transcript.txt") as f:
     lines = "".join(f.readlines())
 f.close()
 
 rna_list = file_utils.split_multiple_sequences(lines)
 rna_list = list_utils.convert_list_to_RNA(rna_list)
-# rna_list[0].to_string()
-# rna_list[0].print_type()
 
-# Creating the gene object
+# ========================= NANOG GENE OBJECT =========================
 nanog_gene = Gene(nanog_seq, rna_list, cds_list, exon_list, prot_list)
-# print(nanog_gene.dna_seq.header)
-# print(nanog_gene.rna_list[0].header)
-# print(nanog_gene.exon_list[0].header)
+# nanog_gene.dna_seq.to_string()                                       # GENE OK
+# nanog_gene.dna_seq.print_type()
+# nanog_gene.rna_list[0].to_string()                                   # mRNA OK
+# nanog_gene.rna_list[0].print_type()
+# nanog_gene.cds_list[0].to_string()                                   # CDS OK
+# nanog_gene.cds_list[0].print_type()
+# nanog_gene.exon_list[0].to_string()                                  # EXON OK
+# nanog_gene.exon_list[0].print_type()
+# nanog_gene.protein_list[0].to_string()                               # PROTEIN OK
+# nanog_gene.protein_list[0].print_type()
+print("nanog gene OK")
+
+# ========================================================================================
+
+#                                  POU5F1 HOMO SAPIENS
+
+# ========================================================================================
+
+print("\n======================== OPEN AND READING POU5F1 GENE FILE ========================\n")
+
+with open(pou5f1_dir + "pou5f1_gene.txt", "r") as f:
+    lines = "".join(f.readlines())
+f.close()
+
+pou5f1_seq = DNA(lines)
+
+# ========================= POU5F1 CDS =========================
+with open(pou5f1_dir + "pou5f1_cds.txt", "r") as f:
+    lines = "".join(f.readlines())
+f.close()
+
+cds_list = file_utils.split_multiple_sequences(lines)
+cds_list = list_utils.convert_list_to_DNA(cds_list)
+
+# ========================= POU5F1 EXON =========================
+with open(pou5f1_dir + "pou5f1_exon.txt", "r") as f:
+    lines = "".join(f.readlines())
+f.close()
+
+exon_list = file_utils.split_multiple_sequences(lines)
+exon_list = list_utils.convert_list_to_DNA(exon_list)
+
+# ========================= POU5F1 PROTEIN =========================
+with open(pou5f1_dir + "pou5f1_prot.txt", "r") as f:
+    lines = "".join(f.readlines())
+f.close()
+
+prot_list = file_utils.split_multiple_sequences(lines)
+prot_list = list_utils.convert_list_to_Protein(prot_list)
+
+# ========================= POU5F1 mRNA =========================
+with open(pou5f1_dir + "pou5f1_transcript.txt", "r") as f:
+    lines = "".join(f.readlines())
+f.close()
+
+rna_list = file_utils.split_multiple_sequences(lines)
+rna_list = list_utils.convert_list_to_RNA(rna_list)
+
+# ========================= POU5F1 GENE OBJECT =========================
+pou5f1_gene = Gene(pou5f1_seq, rna_list, cds_list, exon_list, prot_list)
+# pou5f1_gene.dna_seq.to_string()                                       # GENE OK
+# pou5f1_gene.dna_seq.print_type()
+# pou5f1_gene.rna_list[0].to_string()                                   # mRNA OK
+# pou5f1_gene.rna_list[0].print_type()
+# pou5f1_gene.cds_list[0].to_string()                                   # CDS OK
+# pou5f1_gene.cds_list[0].print_type()
+# pou5f1_gene.exon_list[0].to_string()                                  # EXON OK
+# pou5f1_gene.exon_list[0].print_type()
+# pou5f1_gene.protein_list[0].to_string()                               # PROTEIN OK
+# pou5f1_gene.protein_list[0].print_type()
+print("pou5f1 gene OK")
 
